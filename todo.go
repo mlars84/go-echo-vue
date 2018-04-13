88b7c62b8ef74623f1ce3213ed38bb4306af6c89
Matt Larson <matt.a.larson@gmail.com>
2018-04-13 14:16:29 -0500

    mostly set up routes and sqlite3 db

--- todo.go
+++ todo.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"database/sql"
+	"go-echo-vue/handlers"
+
 	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db := initDB("storage.db")
+	migrate(db)
+
 	e := echo.New()
 
-	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
-	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
-	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+	e.FILE("/", "public/index.html")
+	e.GET("/tasks", handlers.GetTasks(db))
+	e.PUT("/tasks", handlers.PutTask(db))
+	e.DELETE("/tasks/:id", handlers.DeleteTask(db))
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );
+    `
+
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
 }
